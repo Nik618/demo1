e0dccec0b9ece8c87d07ec0bef23000fd2912c8d
Nik618 <trinax01@gmail.com>
2022-02-16 14:13:45 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        System.setProperty("javax.net.ssl.trustStore", "C:\\Program Files\\Java\\jdk-15.0.2\\lib\\security\\cacerts");
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/X509AuthenticationServer.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class X509AuthenticationServer {
+    public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.trustStore", "C:\\Program Files\\Java\\jdk-15.0.2\\lib\\security\\cacerts");
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+        SpringApplication.run(X509AuthenticationServer.class, args);
+    }
+}
